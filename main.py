edf2c4061cdf75e52ece5b82b66c8c8a9da05b71
victorjefferson <vrjefferso33@gmail.com>
2023-02-19 14:35:08 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests as rs
+
+""" input data = url with title of articles and description
+    output = url links written in an email and set to me
+"""
+
+url = "https://finance.yahoo.com"
